03db9519628abee1b97637c32da263b77fe76f5f
k1nase <117951265+k1nase@users.noreply.github.com>
2022-11-16 14:47:09 +0100

    Update openbox_install.sh

--- openbox_install.sh
+++ openbox_install.sh
@@ -2,4 +2,6 @@
 
 sudo cp ~/openbox_subuntu/rc.xml ~/.config/openbox/
 
-sudo cp ~/openbox_subuntu/autostart ~/.config/openbox
+sudo cp ~/openbox_subuntu/autostart ~/.config/openbox/
+
+chmod +x ~/.config/openbox/autostart
